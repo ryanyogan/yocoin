579b304fadafc79e49389677fc22f5e1ee4ab93b
ryan <ryanyogan@gmail.com>
2021-10-17 21:37:30 -0500

    simple dsl for new clients

---
+++ lib/yocoin/exchanges/client.ex
@@ -0,0 +1,113 @@
+defmodule Yocoin.Exchanges.Client do
+  use GenServer
+
+  @type t() :: %__MODULE__{
+          module: module(),
+          conn: pid(),
+          conn_ref: reference(),
+          currency_pairs: [String.t()]
+        }
+
+  @callback exchange_name() :: String.t()
+  @callback server_host() :: list()
+  @callback server_port() :: integer()
+  @callback subscription_frames([String.t()]) :: [{:text, String.t()}]
+  @callback handle_ws_message(map(), any()) :: any()
+
+  defstruct [:module, :conn, :conn_ref, :currency_pairs]
+
+  defmacro defclient(options) do
+    exchange_name = Keyword.fetch!(options, :exchange_name)
+    host = Keyword.fetch!(options, :host)
+    port = Keyword.fetch!(options, :port)
+    currency_pairs = Keyword.fetch!(options, :currency_pairs)
+
+    client_module = __MODULE__
+
+    quote do
+      @behaviour unquote(client_module)
+      import unquote(client_module), only: [validate_required: 2]
+      require Logger
+
+      def available_currency_pairs, do: unquote(currency_pairs)
+      def exchange_name, do: unquote(exchange_name)
+      def server_host, do: unquote(host)
+      def server_port, do: unquote(port)
+
+      def handle_ws_message(msg, state) do
+        Logger.debug("handle_ws_message: #{inspect(msg)}")
+        {:noreply, state}
+      end
+
+      defoverridable(handle_ws_message: 2)
+    end
+  end
+
+  def start_link(module, currency_pairs, options \\ []) do
+    GenServer.start_link(__MODULE__, {module, currency_pairs}, options)
+  end
+
+  @impl true
+  def init({module, currency_pairs}) do
+    client = %__MODULE__{
+      module: module,
+      currency_pairs: currency_pairs
+    }
+
+    {:ok, client, {:continue, :connect}}
+  end
+
+  @impl true
+  def handle_continue(:connect, client) do
+    {:noreply, connect(client)}
+  end
+
+  @impl true
+  def handle_info({:gun_up, conn, :http}, %{conn: conn} = client) do
+    :gun.ws_upgrade(conn, "/")
+    {:noreply, client}
+  end
+
+  def handle_info({:gun_upgrade, conn, _ref, ["websocket"], _headers}, %{conn: conn} = client) do
+    subscribe(client)
+    {:noreply, client}
+  end
+
+  def handle_info({:gun_ws, conn, _ref, {:text, msg}}, %{conn: conn} = client) do
+    handle_ws_message(Jason.decode!(msg), client)
+  end
+
+  def connect(client) do
+    host = server_host(client.module)
+    port = server_port(client.module)
+    {:ok, conn} = :gun.open(host, port, %{protocols: [:http]})
+    conn_ref = Process.monitor(conn)
+    %{client | conn: conn, conn_ref: conn_ref}
+  end
+
+  defp server_host(module), do: module.server_host()
+  defp server_port(module), do: module.server_port()
+
+  defp subscribe(client) do
+    subscription_frames(client.module, client.currency_pairs)
+    |> Enum.each(&:gun.ws_send(client.conn, &1))
+  end
+
+  defp subscription_frames(module, currency_pairs) do
+    module.subscription_frames(currency_pairs)
+  end
+
+  defp handle_ws_message(msg, client) do
+    module = client.module
+    module.handle_ws_message(msg, client)
+  end
+
+  @spec validate_required(map(), [String.t()]) :: :ok | {:error, {String.t(), :required}}
+  def validate_required(msg, keys) do
+    required_key = Enum.find(keys, fn k -> is_nil(msg[k]) end)
+
+    if is_nil(required_key),
+      do: :ok,
+      else: {:error, {required_key, :required}}
+  end
+end
--- lib/yocoin/exchanges/coinbase_client.ex
+++ lib/yocoin/exchanges/coinbase_client.ex
@@ -1,54 +1,16 @@
 defmodule Yocoin.Exchanges.CoinbaseClient do
-  use GenServer
   alias Yocoin.{Ticker, Trade}
-  @exchange_name "coinbase"
+  alias Yocoin.Exchanges.Client
+  require Client
 
-  def start_link(currency_pairs, options \\ []) do
-    GenServer.start_link(__MODULE__, currency_pairs, options)
-  end
-
-  @spec validate_required(map(), [String.t()]) :: :ok | {:error, {String.t(), :required}}
-  def validate_required(msg, keys) do
-    required_key = Enum.find(keys, fn k -> is_nil(msg[k]) end)
-
-    if is_nil(required_key),
-      do: :ok,
-      else: {:error, {required_key, :required}}
-  end
+  Client.defclient(
+    exchange_name: "coinbase",
+    host: 'ws-feed.pro.coinbase.com',
+    port: 443,
+    currency_pairs: ["BTC-USD", "ETH-USD", "LTC-USD", "BTC-EUR", "ETH-EUR", "LTC-EUR"]
+  )
 
   @impl true
-  def init(currency_pairs) do
-    state = %{
-      currency_pairs: currency_pairs,
-      conn: nil
-    }
-
-    {:ok, state, {:continue, :connect}}
-  end
-
-  @impl true
-  def handle_continue(:connect, state) do
-    updated_state = connect(state)
-    {:noreply, updated_state}
-  end
-
-  @impl true
-  def handle_info({:gun_up, conn, :http}, %{conn: conn} = state) do
-    :gun.ws_upgrade(state.conn, "/")
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_info({:gun_upgrade, conn, _ref, ["websocket"], _headers}, %{conn: conn} = state) do
-    subscribe(state)
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_info({:gun_ws, conn, _ref, {:text, msg}}, %{conn: conn} = state) do
-    handle_ws_message(Jason.decode!(msg), state)
-  end
-
   def handle_ws_message(%{"type" => "ticker"} = msg, state) do
     msg
     |> message_to_trade()
@@ -57,42 +19,12 @@ defmodule Yocoin.Exchanges.CoinbaseClient do
     {:noreply, state}
   end
 
-  # TODO: Wipe your butt
   def handle_ws_message(msg, state) do
     IO.inspect(msg, label: "unhandled message")
     {:noreply, state}
   end
 
-  def message_to_trade(msg) do
-    with :ok <- validate_required(msg, ["product_id", "time", "price", "last_size"]),
-         {:ok, traded_at, _} <- DateTime.from_iso8601(msg["time"]) do
-      currency_pair = msg["product_id"]
-
-      Trade.new(
-        ticker: Ticker.new(@exchange_name, currency_pair),
-        price: msg["price"],
-        volume: msg["last_size"],
-        traded_at: traded_at
-      )
-    else
-      {:error, _reason} = error ->
-        error
-    end
-  end
-
-  def server_host, do: 'ws-feed.pro.coinbase.com'
-  def server_port, do: 443
-
-  def connect(state) do
-    {:ok, conn} = :gun.open(server_host(), server_port(), %{protocols: [:http]})
-    %{state | conn: conn}
-  end
-
-  defp subscribe(state) do
-    subscription_frames(state.currency_pairs)
-    |> Enum.each(&:gun.ws_send(state.conn, &1))
-  end
-
+  @impl true
   def subscription_frames(currency_pairs) do
     msg =
       %{
@@ -103,5 +35,23 @@ defmodule Yocoin.Exchanges.CoinbaseClient do
       |> Jason.encode!()
 
     [{:text, msg}]
+  end
+
+  @spec message_to_trade(map()) :: {:ok, Trade.t()} | {:error, any()}
+  def message_to_trade(msg) do
+    with :ok <- validate_required(msg, ["product_id", "time", "price", "last_size"]),
+         {:ok, traded_at, _} <- DateTime.from_iso8601(msg["time"]) do
+      currency_pair = msg["product_id"]
+
+      Trade.new(
+        ticker: Ticker.new(exchange_name(), currency_pair),
+        price: msg["price"],
+        volume: msg["last_size"],
+        traded_at: traded_at
+      )
+    else
+      {:error, _reason} = error ->
+        error
+    end
   end
 end
